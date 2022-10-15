6e7781c9aa313c7baeffe51acf2696e41a1f163c
Michael Matheus <michaelmatheus2001@gmail.com>
2022-10-15 08:05:17 -0300

    feat: radix ui

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -5,9 +5,30 @@ export default {
     title: 'Components/Text',
     component: Text,
     args: {
-        children: 'Lorem Ipsum',
-        size: 'lg'
+        children: 'Lorem Ipsum'
     }
 } as Meta<TextProps>
 
-export const Default: StoryObj = {}
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Testando</p>
+        )
+    }
+}
